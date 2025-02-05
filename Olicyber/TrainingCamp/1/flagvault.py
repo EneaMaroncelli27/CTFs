fb60ffd515a089a5e46339022f0e18b958dbe30b
Enea Maroncelli <mururabdul@gmail.com>
2025-02-05 22:55:47 +0100

    Delete Olicyber directory

--- Olicyber/TrainingCamp/1/flagvault.py
+++
@@ -1,8 +0,0 @@
-import angr
-proj = angr.Project("./flagvault",
-    load_options={"auto_load_libs":False},
-    main_opts={"base_addr":0})
-init = proj.factory.entry_state()
-sim = proj.factory.simulation_manager(init)
-s = sim.explore(find=0x1437 , avoid=0x13E1)
-print(s.found[0].posix.dumps(0).decode(), end="")
--- Olicyber/TrainingCamp/3/solo_una_convenzione.py
+++
@@ -1,5 +0,0 @@
-
-import requests
-url='http://convenzione.challs.olicyber.it/'
-r = requests.request('FLAG' ,url=url)
-print(r.headers)
